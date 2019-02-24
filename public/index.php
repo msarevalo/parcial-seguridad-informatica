a439ddb488726b507d1c771681e95f860679a50e
msarevalo <msarevalo@github.com>
2019-02-24 10:51:03 -0500

    inicio con estilos

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Parcial | Seguridad Informatica</title>
+    <meta charset="UTF-8">
+    <!-- Estilos -->
+    <link href="../css/estilos.css" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../js/script.js" type="application/javascript"></script>
+</head>
+<body>
+
+<div style="margin-left: 350px">
+    <a class="btn w-M br-0 stl-3" href="#">Cargar Archivo</a>
+    <a class="btn w-M br-0 stl-3" href="#">Historico</a>
+</div>
+</body>
+</html>
