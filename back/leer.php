a4163831725848e470d59e0be8cc87cbe8db0b9d
msarevalo <msarevalo@github.com>
2019-02-26 00:46:16 -0500

    lectura de archivo y clasificacion de palabras

--- back/leer.php
+++ back/leer.php
@@ -14,32 +14,34 @@ try {
     //echo $tipo;
     $contadorPalabras = 0;
     $contadorLetras = 0;
-    $prueba[] = 0;
+    $letraspalabras[] = 0;
     if ($tipo==="text/plain"){
         foreach ($archivo as $ar => $archi) {
             //echo $achi . "<br>";
             $datos = explode(" ", $archi);
-            for ($i=0;$i<sizeof($datos);$i++){
+            for ($i = 0;$i < sizeof($datos);$i++){
                 $letras = strlen($datos[$i]);
                 echo $i . " " . $datos[$i] . " " . $letras;
-                if (in_array($letras, $prueba)){
-                    $prueba[$letras] = $prueba[$letras]+1;
-                    echo " " . $prueba[$letras] . "<br>";
+                if (isset($letraspalabras[$letras])){
+                    $letraspalabras[$letras] = $letraspalabras[$letras]+1;
+                    echo " " . $letraspalabras[$letras] . "<br>";
+                    //echo var_dump($letraspalabras) . "<br>";
                 }else{
-                    //echo "no esta";
-                    //$prueba[$letras] = 1;
-                    array_push($prueba, $letras);
-                    $prueba[$letras] = 1;
-                    echo " " . $prueba[$letras] . "<br>";
+                    echo "el " . $letras . " no esta";
+                    //$letraspalabras[$letras] = 1;
+                    //array_push($letraspalabras, $letras);
+                    $letraspalabras[$letras] = 1;
+                    echo " " . $letraspalabras[$letras] . "<br>";
+                    //echo var_dump($letraspalabras) . "<br>";
                 }
                 $contadorLetras = $contadorLetras + $letras;
                 $contadorPalabras ++;
             }
         }
     }
-    echo  "el archivo contiene " . $contadorPalabras . " palabras<br>";
-    echo "ademas de contar con " . $contadorLetras . "letras<br><br>";
-    echo var_dump($prueba);
+    echo  "<br>el archivo contiene " . $contadorPalabras . " palabras<br>";
+    echo "ademas de contar con " . $contadorLetras . " letras<br><br>";
+    echo var_dump($letraspalabras);
 }catch (Exception $e){
     echo "<script>alert('Algo ha pasado, verifica tu archivo'); window.location.href='../views/importar-horario.php'</script>";
 }
